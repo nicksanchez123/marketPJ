d3ce8d7b582a9f463f482021a4b047fdfcf03a15
nicksanchez123 <nicolasborgesviana1@gmail.com>
2025-06-07 21:05:47 -0300

    ebaaa linduu

--- JS/index.js
+++ JS/index.js
@@ -1,4 +1,5 @@
-const signUpButton = document.getElementById('signUp');
+const signUpButton = document.getElementById('signUp1');
+const signUpButton1 = document.getElementById('signUp');
 const signInButton = document.getElementById('signIn');
 const container = document.getElementById('container');
 
@@ -9,7 +10,13 @@ signUpButton.addEventListener('click', () => {
 signInButton.addEventListener('click', () => {
 	container.classList.remove("right-panel-active");
 });
+signUpButton1.addEventListener('click', () => {
+	container.classList.add("right-panel-active");
+});
+
 const register = document.getElementById('registrar');
 register.addEventListener('click', () => {
 	container.classList.remove("right-panel-active");
 });
+const signUpMobile = document.getElementById('signUpMobile');
+
